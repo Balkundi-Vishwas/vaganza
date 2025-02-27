f355117520b5f5fb4d4e89ffe8825be16be171e1
vishwas <vishwasbalkundi06@gmail.com>
2025-02-27 17:33:49 +0530

    added tooltip

--- src/components/Teamlist.jsx
+++ src/components/Teamlist.jsx
@@ -1,13 +1,32 @@
 import teamsData from "../../data.json";
+import { useState } from "react";
+
+import Tooltip from '@mui/material/Tooltip';
 
 const TeamList = () => {
+  const [activeTooltip, setActiveTooltip] = useState(null);
+
+  const handleTooltipOpen = (index) => {
+    setActiveTooltip(activeTooltip === index ? null : index);
+  };
   return (
     <div>
       <h2 className="text-center text-2xl font-bold mt-5">🏅 Participating Teams</h2>
       <div className="flex flex-wrap justify-center mt-5">
-        {teamsData.teams.map((team, index) => (
+      {teamsData.teams.map((team, index) => (
           <div key={index} className="m-5 text-center">
-            <img src={`/images/${team.name.replace(" ", "%20")}.jpeg`} alt={team.name} className="w-32 h-32 object-contain mx-auto rounded-lg shadow-md" />
+            <Tooltip
+              title={team.description}
+              placement="bottom"
+              open={activeTooltip === index}
+            arrow>
+              <img
+                src={`/images/${team.name.replace(" ", "%20")}.jpeg`}
+                onClick={() => handleTooltipOpen(index)}
+                alt={team.name}
+                className="w-32 h-32 object-contain mx-auto rounded-lg shadow-md cursor-pointer"
+              />
+            </Tooltip>
             <h3 className="mt-2 text-lg font-semibold">{team.name}</h3>
           </div>
         ))}
